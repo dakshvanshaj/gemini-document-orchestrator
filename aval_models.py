18da4ab6947c2c28533af02a2c79086cc2287a76
dakshvanshaj <davevanshaj55646@gmail.com>
2026-02-05 17:18:53 +0000

    feat: script to see available models and their details

---
+++ aval_models.py
@@ -0,0 +1,13 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+print("Searching for available models...")
+for m in client.models.list():
+    if "generateContent" in m.supported_actions:
+        print(f"✅ Found: {m.name}")
